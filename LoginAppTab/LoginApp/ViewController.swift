628e099141fb778629cc682f3daf001b173e9ff2
Darlene Phan <darlenephan@gmail.com>
2016-04-09 16:24:41 +1000

    Updated profile

---
+++ LoginAppTab/LoginApp/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Darlene Phan on 4/5/16.
+//  Copyright © 2016 Darlene Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var textFieldUsername: UITextField!
+    
+    
+    
+    
+    
+    @IBOutlet weak var textFieldPassword: UITextField!
+    
+    
+    @IBAction func buttonLogin(sender: AnyObject)
+    {
+      
+    
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
